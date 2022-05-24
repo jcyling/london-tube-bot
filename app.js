acc822d53ea4cd2bb7b281366fe5c50ef2e1f0af
jcyling <jane.cy.ling@gmail.com>
2022-05-24 19:30:27 +0800

    Add individual line query

--- app.js
+++ app.js
@@ -1,57 +1,97 @@
-require('dotenv').config();
+require("dotenv").config();
 const fetch = require("node-fetch");
-const { Telegraf } = require('telegraf');
-const { Keyboard } = require('telegram-keyboard');
+const { Telegraf } = require("telegraf");
+const { Keyboard } = require("telegram-keyboard");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const baseUrl = "https://api.tfl.gov.uk/";
 
-// API call function
+// Tube lines
+const tubeLines = [
+  "Bakerloo",
+  "Central",
+  "Circle",
+  "District",
+  "Hammersmith & City",
+  "Jubilee",
+  "Metropolitan",
+  "Northern",
+  "Piccadilly",
+  "Victoria",
+  "Waterloo",
+  "Overground",
+  "TfL Rail",
+  "DLR"
+];
+
+// Api call
 async function getCall(url) {
-  const response = await fetch(url)
-  const json = await response.json()
-  return json
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
 }
 
-async function getLines() {
-  const response = getCall(url + 'line/mode/tube')
-  return response.map(item => item.name)
+async function getLineStatus(line) {
+  const response = await fetch(baseUrl + "line/mode/tube/status");
+  const data = await response.json();
+  const lineData = data.filter(item => item.name === line);
+  const status = lineData[0].lineStatuses[0].statusSeverityDescription;
+  return `${line}: ${status}`;
 }
 
-bot.command('start', (ctx) => {
-  console.log(ctx.from)
-  bot.telegram.sendMessage(ctx.chat.id, 'Hello there! Welcome to the London Tube Bot', {})
-})
+bot.command("start", (ctx) => {
+  bot.telegram.sendMessage(ctx.chat.id, "Hello there! Welcome to the London Tube Bot", {});
+});
 
-bot.command('check', async (ctx) => {
+bot.command("check", async (ctx) => {
   const url = baseUrl + "line/mode/tube/status";
 
-  const data = await getCall(url)
-  console.log(data)
+  const data = await getCall(url);
 
   const lineNames = data.map(item => item.name);
   const lineStatus = data.map(item => item.lineStatuses[0].statusSeverityDescription);
-  const lineStatusNum = data.map(item => item.lineStatuses[0].statusSeverity);
+  // const lineStatusNum = data.map(item => item.lineStatuses[0].statusSeverity);
 
   const status = [];
   lineNames.forEach((line, index) => {
     status.push(`${line}: ${lineStatus[index]}`);
   });
 
-  const message = status.join('\r\n');
+  const message = status.join("\r\n");
 
-  ctx.reply(message)
+  ctx.reply(message);
 });
 
-bot.on('text', async (ctx) => {
-  const lines = getLines()
+bot.command("lines", async (ctx) => {
   const keyboard = Keyboard.make([
-    'Bakerloo', 'Central', 'Circle', 'District', 'Hammersmith & City', 'Jubilee', 'Metropolitan', 'Northern', 'Piccadilly', 'Victoria', 'Water', 'London Overground', 'TfL Rail', 'DLR'    
-  ])
+    ["Bakerloo", "Central", "Circle"],
+    ["District", "Hammersmith & City", "Jubilee"],
+    ["Metropolitan", "Northern", "Piccadilly"],
+    ["Victoria", "Waterloo"],
+    ["Overground", "TfL Rail", "DLR"]
+  ]).reply();
 
-  await ctx.reply('Choose your line', keyboard.reply())
-  await ctx.reply('Choose your line', keyboard.inline())
+  ctx.reply("Pick a line", keyboard);
 
-})
+});
+
+bot.hears(tubeLines, async (ctx) => {
+  ctx.reply("Hold on, checking...");
+  const status = await getLineStatus(ctx.message.text);
+  ctx.reply(status);
+});
+
+// Handle other texts sent to bot
+bot.on("text", async (ctx) => {
+  const message = `
+  Welcome to the London Tube Bot!
+  Get updates by sending these commands:
+  /check - Check all lines
+  /lines - Choose a line to check
+  "Central" - Enter line name to check
+  `;
+
+  ctx.reply(message);
+});
 
 bot.launch();
