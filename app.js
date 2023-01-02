a0246a191028c943d52db99a913143e8a220f0e8
Jane Ching Yee Ling <jane.cy.ling@gmail.com>
2023-01-02 15:13:33 +0000

    Change variable to cyclic

--- app.js
+++ app.js
@@ -11,15 +11,18 @@ app.use(morgan("tiny"));
 if (process.env.NODE_ENV === "development") {
   bot.launch();
   // bot.telegram.setWebhook(`${process.env.LT_URL}${process.env.BOT_TOKEN}`);
-}
-else if (process.env.NODE_ENV === "production") {
-  bot.telegram.setWebhook(`${process.env.HEROKU_URL}${process.env.BOT_TOKEN}`);
+} else if (process.env.NODE_ENV === "production") {
+  bot.telegram.setWebhook(`${process.env.CYCLIC_URL}${process.env.BOT_TOKEN}`);
 }
 
 app.use(bot.webhookCallback(`/${process.env.BOT_TOKEN}`));
 
 app.get("/", (req, res) => {
-  return res.status(200).send("Welcome to the London Tube Bot API. Find this bot on Telegram @LondonTubeBot.");
+  return res
+    .status(200)
+    .send(
+      "Welcome to the London Tube Bot API. Find this bot on Telegram @LondonTubeBot."
+    );
 });
 
 app.listen(process.env.PORT || 3000, () => {
